8815c3a1ab3c776a73188c1d415b17ad1733af45
박해름 <165573143+rumi8048@users.noreply.github.com>
2024-05-13 22:22:21 +0900

    Add files via upload

---
+++ src/main/java/com/example/study/controller/calculator/CalculatorController.java
@@ -0,0 +1,29 @@
+package com.example.study.controller.calculator;
+
+
+import com.example.study.controller.dto.calculator.CalculatorAddRequest;
+import com.example.study.controller.dto.calculator.CalculatorMultiplyRequest;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class CalculatorController {
+    @PostMapping("/add")
+    public int addTwoNumbers( @RequestBody CalculatorAddRequest request) {
+        return request.getNumber1() + request.getNumber2();
+    }
+
+    @PostMapping("/multiply")
+    public int multiplyTwoNumbers(@RequestBody CalculatorMultiplyRequest request) {
+        return request.getNumber1() * request.getNumber2();
+    }
+    @GetMapping("/calc")
+    public String calc(@RequestParam int number1, @RequestParam int number2) {
+        int add= number1+number2;
+        int minus = number1 - number2;
+        int multiply = number1 * number2;
+        double division = number1 / (double) number2;
+
+        return "add = "+ add+ "minus = "+minus+ "multiply = "+multiply + "division = "+division ;
+    }
+
+}
